362c9d272ee4be4a2f9ba4801876e32384b1d482
Damien Touchette <djtouchette1993@gmail.com>
2016-12-23 17:22:08 -0700

    Quick refactoring

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -1,15 +1,22 @@
 import jwt from 'jsonwebtoken';
 
+const NO_TOKEN = {
+		success: false, 
+		message: 'No token provided.'
+	};
+
+const FAILED_AUTH = { 
+	success: false, 
+	content: 'Failed to authenticate JWT.' 
+};
+
 export default function(req, res, next) {
 	const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-	if (!token) return res.status(403).send({
-		success: false, 
-		message: 'No token provided.'
-	});
+	if (!token) return res.status(403).send(NO_TOKEN);
 
 	jwt.verify(token, process.env.SECRET, function(err, decoded) {
-		if (err) return res.status(403).json({ success: false, content: 'Failed to authenticate JWT.' });
+		if (err) return res.status(403).json(FAILED_AUTH);
 
 		req.decoded = decoded;
 		next();
